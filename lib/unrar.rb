9c5a619d062eedfcc0a0e54d124c3e0418b3617b
JR Boyens <jr.boyens@chacha.com>
2012-04-16 21:48:28 -0700

    Some basic implementation

--- lib/unrar.rb
+++ lib/unrar.rb
@@ -1,5 +1,43 @@
 require "unrar/version"
+require 'fileutils'
+require 'tmpdir'
 
 module Unrar
-  # Your code goes here...
+  class Archive
+    SEARCH_PATH = ["/usr/local/bin", "/usr/bin", "/bin", "/opt/local/bin"]
+
+    attr_accessor :file, :tmpdir
+
+    def initialize(file)
+      self.file = file
+      self.tmpdir = Dir.mktmpdir
+
+      # shitty clean up; fix this
+      at_exit {
+        FileUtils.rm_rf self.tmpdir
+      }
+
+      unless File.readable? file
+        raise "Cannot read #{file}!"
+      end
+    end
+
+    def extract filename, *filenames
+      `#{Archive.unrar} -y x #{self.file} #{filename} #{filenames.join(" ")} #{tmpdir}/`
+
+      Dir["#{tmpdir}/**/*"].to_ary
+    end
+
+    def self.unrar
+      @@unrar ||= search_for "unrar"
+    end
+
+    private
+
+    def self.search_for file
+      SEARCH_PATH.find do |path|
+        return "#{path}/#{file}" if File.exists? "#{path}/#{file}"
+      end
+    end
+  end
 end
---
+++ spec/lib/unrar_spec.rb
@@ -0,0 +1,41 @@
+require 'unrar'
+
+describe Unrar::Archive do
+  describe "finding the unrar executable" do
+    before do
+      Unrar::Archive.class_variable_set(:@@unrar, nil)
+    end
+
+    context "on search path" do
+      before do
+        Unrar::Archive.should_receive(:search_for).with("unrar").and_return("/pants/unrar")
+      end
+
+      it "should find unrar" do
+        Unrar::Archive.unrar.should == "/pants/unrar"
+      end
+    end
+
+    context "not on search path" do
+      before do
+        Unrar::Archive.should_receive(:search_for).with("unrar").and_return(nil)
+      end
+
+      it "should not find unrar" do
+        Unrar::Archive.unrar.should be_nil
+      end
+    end
+  end
+
+  describe "should be able to extract" do
+    subject { Unrar::Archive.new('spec/fixtures/test.rar') }
+
+    it "a single file" do
+      subject.extract('001.jpg').should_not be_nil
+    end
+
+    it "multiple files" do
+      subject.extract('001.jpg', '002.jpg').should have_exactly(2).items
+    end
+  end
+end
